64199660c68586a4c04df1712dbf8b5b36cafc33
Sara Habel <digitalsara@icloud.com>
2018-03-26 09:34:15 -0400

    Add content to the cards

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/sarahabel/Dropbox/Algonquin/6-2018 - GD/Web Dev 4 • DSN1678/ecommerce-pattern-library
- * @version 1522068491117
+ * @version 1522071237297
  */
-const patternManifest_1522068491116 = {
+const patternManifest_1522071237297 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -1354,5 +1354,5 @@ const patternManifest_1522068491116 = {
   }
 };
 
-patternBotIncludes(patternManifest_1522068491116);
+patternBotIncludes(patternManifest_1522071237297);
 }());
